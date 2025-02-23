5f41e6f20ab5e5345ab44313d5812bf17b9d264f
bruno bd <bruno.donati@hotmail.fr>
2025-02-23 22:40:48 +0100

    Ajoutez des fichiers projet.

---
+++ bibliotheque/bibliotheque.cpp
@@ -0,0 +1,12 @@
+﻿// bibliotheque.cpp : définit le point d'entrée de l'application.
+//
+
+#include "bibliotheque.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ bibliotheque/src/main.cpp
@@ -0,0 +1,12 @@
+// Cours de Jason Champagne : Formationvideo - bibliothéque dynamique - https://www.youtube.com/watch?v=pKOJFZN_HUk
+// 
+// Programme principal
+
+#include "../include/point.h"
+#include "../include/screen.h"
+#include <stdio.h>
+
+int main(void) 
+{
+
+}
